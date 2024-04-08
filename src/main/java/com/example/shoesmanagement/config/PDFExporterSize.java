84ce77ee0f44e36357a56b2c56954eb3b4f7f551
Le Dang Thuc <thucldph22733@fpt.edu.vn>
2024-04-08 21:40:13 +0700

    i

---
+++ src/main/java/com/example/shoesmanagement/config/PDFExporterSize.java
@@ -0,0 +1,82 @@
+package com.example.shoesmanagement.config;
+
+import com.example.shoesmanagement.model.Size;
+import com.lowagie.text.*;
+import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.List;
+
+public class PDFExporterSize {
+    private List<Size> listSize;
+
+    public PDFExporterSize(List<Size> listSize) {
+        this.listSize = listSize;
+    }
+
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.PINK);
+        cell.setPadding(5);
+
+        com.lowagie.text.Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(Color.WHITE);
+
+        cell.setPhrase(new Phrase("ID Size", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Ma Size", font));
+
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("So Size", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Trang Thai", font));
+        table.addCell(cell);
+
+    }
+
+    private void writeTableData(PdfPTable table) {
+        for (Size size : listSize) {
+            table.addCell(String.valueOf(size.getIdSize()));
+            table.addCell(size.getMaSize());
+            table.addCell(String.valueOf(size.getSoSize()));
+            String trangThaiText = (size.getTrangThai() == 1) ? "Hoat Dong" : "Khong Hoat Dong";
+            table.addCell(trangThaiText);
+        }
+    }
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(18);
+        font.setColor(Color.BLACK);
+
+        Paragraph p = new Paragraph("List of Sizes", font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+
+        document.add(p);
+
+        PdfPTable table = new PdfPTable(4);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[]{3.5f, 1.5f, 1.5f, 3.0f});
+        table.setSpacingBefore(10);
+
+        writeTableHeader(table);
+        writeTableData(table);
+
+        document.add(table);
+
+        document.close();
+
+    }
+}
---
+++ src/main/java/com/example/shoesmanagement/service/SizeService.java
@@ -0,0 +1,25 @@
+package com.example.shoesmanagement.service;
+
+import com.example.shoesmanagement.model.Size;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
+public interface SizeService {
+    public List<Size> getAllSizeActiveOrderByName();
+
+    public List<Size> getAllSize();
+
+    public void save(Size size);
+
+    public void deleteByIdSize(UUID id);
+
+    public Size getByIdSize(UUID id);
+
+    public List<Size> getByActive();
+
+    public List<Size> filterSizes(Integer selectedSize, String maSize);
+
+    public void importDataFromExcel(InputStream excelFile);
+}
